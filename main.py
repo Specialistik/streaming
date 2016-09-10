90b08d921b897febbc2f28814a038d3755aeab1f
Specialistik <test04@list.ru>
2016-09-10 04:01:26 +0300

    слайдер и бутстрап

--- main.py
+++ main.py
@@ -16,8 +16,7 @@
 from flask import Flask, render_template, Response
 from camera import VideoCamera
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_folder='static')
 @app.route('/')
 def index():
     return render_template('index.html')
