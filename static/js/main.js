78da69802cf7b6ec299fa5166ec906e010585390
Specialistik <test04@list.ru>
2016-09-11 15:00:25 +0300

    убитые доработки
    Signed-off-by: Specialistik <test04@list.ru>

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
+    //$('.carousel').carousel('pause');
     $('.video').click(function(){this.paused?this.play():this.pause();});
+
+    if (window.chrome)
+    $("[type=video\\\/mp4]").each(function()
+    {
+        $(this).attr('src', $(this).attr('src').replace(".mp4", "_c.mp4"));
+    });
 });
