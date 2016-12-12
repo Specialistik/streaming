4a0aac552e8ca3583717343f295d369e6802cffe
root <root@krevedko.krevedko>
2016-12-12 22:10:29 +0300

    доработки по внутреннему трекеру

--- abstract_crm/models.py
+++ abstract_crm/models.py
@@ -1,27 +1,36 @@
 #coding: utf-8
 
 import os
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 from core.models import List
 
+class Project(List):
+    class Meta:
+       db_table = 'projects'
+       verbose_name = u'Проект'
+       verbose_name_plural = u'Проекты'
+
 
 class TaskStatus(List):
     class Meta:
        db_table = 'task_statuses'
-       verbose_name = u'task status'
-       verbose_name_plural = u'task statuses'
+       verbose_name = u'Статус задачи'
+       verbose_name_plural = u'Статусы задач'
 
 
 
 class Task(models.Model):
     title = models.CharField(max_length=80, verbose_name=u"Заголовок")
     description = RichTextField(verbose_name=u"Описание")
-    user = models.ForeignKey(User, verbose_name=u'django user') 
-    status = models.ForeignKey(TaskStatus, verbose_name=u"task status")
-    time_creation = models.DateTimeField(null=True, blank=True, verbose_name=u'task creation time')
-    time_end = models.DateTimeField(null=True, blank=True, verbose_name=u'task end time')
+    project = models.ForeignKey(Project, verbose_name=u'Проект') 
+    user = models.ForeignKey(User, verbose_name=u'Пользователь') 
+    status = models.ForeignKey(TaskStatus, verbose_name=u"Статус")
+    time_creation = models.DateTimeField(default=datetime.now(), verbose_name=u'Время создания')
+    time_end = models.DateTimeField(null=True, blank=True, verbose_name=u'Время завершения')
 
 
     def __repr__(self):
@@ -36,7 +45,7 @@ class Task(models.Model):
 
     class Meta:
        db_table = 'tasks'
-       verbose_name = u'task'
-       verbose_name_plural = u'tasks'
+       verbose_name = u'Задача'
+       verbose_name_plural = u'Задачи'
 
 
