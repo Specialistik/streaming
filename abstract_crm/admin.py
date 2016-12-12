524b54bb81ecefc222dd15fa5ee8b056e6793dee
root <root@krevedko.krevedko>
2016-12-12 22:25:21 +0300

    Приоритеты задач

--- abstract_crm/admin.py
+++ abstract_crm/admin.py
@@ -1,7 +1,9 @@
 #coding: utf-8
 
 from django.contrib import admin
-from models import Task, TaskStatus
+from models import Task, TaskStatus, Project, Priority
 
 admin.site.register(Task)
 admin.site.register(TaskStatus)
+admin.site.register(Project)
+admin.site.register(Priority)
