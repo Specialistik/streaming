d4e99b1157baaf0f66a982c0ad977ec432428db4
Specialistik <test04@list.ru>
2016-12-13 17:32:18 +0000

    editable admin panel grids

--- abstract_crm/admin.py
+++ abstract_crm/admin.py
@@ -3,7 +3,13 @@
 from django.contrib import admin
 from models import Task, TaskStatus, Project, Priority, Tracker
 
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('user', 'status', 'priority')
+    list_editable = ('title', 'user', 'status', 'priority')
+
+
+admin.site.register(Task, TaskAdmin)
 #admin.site.register(TaskStatus)
 admin.site.register(Project)
 admin.site.register(Tracker)
