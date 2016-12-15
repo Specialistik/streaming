b7ab4967527139bcc7fa88ed57c434238d98320d
specialistik <specialistik@krevedko.krevedko>
2016-12-16 02:28:12 +0300

    доработки по дизайну главной страницы, левой части и навигации

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 #coding: utf-8
 
 from django.contrib import admin
-from core.models import Article, Profession
+from core.models import Article, Profession, Status
 
 admin.site.register(Article)
 admin.site.register(Profession)
+#admin.site.register(Status)
