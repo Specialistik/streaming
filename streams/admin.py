6c09340b9a6790659ac42c24c197f992be2a17c6
specialistik <specialistik@krevedko.krevedko>
2016-12-16 17:58:26 +0300

    Дизайн главной, левой и правой части и гриды в админке приведены к общему виду

--- streams/admin.py
+++ streams/admin.py
@@ -1,9 +1,21 @@
 #coding: utf-8
 
 from django.contrib import admin
-from streams.models import VideoStreamSet, VideoStream, AudioStream
+from streams.models import VideoStreamSet, VideoStream
 
-admin.site.register(VideoStreamSet)
-admin.site.register(VideoStream)
-#admin.site.register(AudioStream)
+
+class VideoStreamSetAdmin(admin.ModelAdmin):
+    exclude = ('keywords',)
+    list_display = ('title', 'url')
+    list_editable = ('url',)
+
+
+class VideoStreamAdmin(admin.ModelAdmin):
+    exclude = ('keywords',)
+    list_display = ('title', 'url')
+    list_editable = ('url',)
+
+
+admin.site.register(VideoStreamSet, VideoStreamSetAdmin)
+admin.site.register(VideoStream, VideoStreamAdmin)
 
--- videos/admin.py
+++ videos/admin.py
@@ -4,7 +4,25 @@ from django.contrib import admin
 from videos.models import VideoCategory, Author, Video
 
 
-admin.site.register(VideoCategory)
-admin.site.register(Author)
-admin.site.register(Video)
+class VideoCategoryAdmin(admin.ModelAdmin):
+    exclude = ('keywords',)
+    list_display = ('title', 'url')
+    list_editable = ('url',)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    exclude = ('keywords',)
+    list_display = ('title', 'url', 'profession', 'image', 'status')
+    list_editable = ('url',)
+
+
+class VideoAdmin(admin.ModelAdmin):
+    exclude = ('keywords',)
+    list_display = ('title', 'url')
+    list_editable = ('url',)
+
+
+admin.site.register(VideoCategory, VideoCategoryAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Video, VideoAdmin)
 
