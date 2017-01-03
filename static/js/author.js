5ea48932d61216efad74542d85460f190b42b9fe
specialistik <specialistik@krevedko.krevedko>
2017-01-03 19:11:50 +0300

    прикрутилась наконец нормальная карусель

--- static/js/author.js
+++ static/js/author.js
@@ -1,16 +1,10 @@
 $(function(){
-    $('.pic-wrapper a').click(function(e){
-    
-        var that = this;
-        e.preventDefault();
-        console.log($('#video'));
-		$('source').attr("src", $(that).attr('href'));
-        $('video').play();
-    });
-    
-    $('.multiple-items').slick({
+	$('.jcarousel').jcarousel();
+/*
+    $('.fancy-shmancy-slider').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3
     });
+*/
 });
